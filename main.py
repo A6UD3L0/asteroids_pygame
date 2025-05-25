cd8fc3a08cb2d64665888060fd547d806cb1a77a
Juan Felipe Agudelo Rios <juanf.agudelor@gmail.com>
2025-05-25 10:53:53 -0500

    CH2:Player L2:Game Loop with loop

--- main.py
+++ main.py
@@ -13,9 +13,13 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     run=True
     while run: 
-        screen.fill(color="Black")
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False 
+        screen.fill("black")
+        pygame.display.flip()
 
-
+    pygame.quit()
 
 
 if __name__ == "__main__":
