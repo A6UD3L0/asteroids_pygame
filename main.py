725c4dfaa111bdca3e258c58feaa77ffc4cf15ec
Juan Felipe Agudelo Rios <juanf.agudelor@gmail.com>
2025-05-27 08:31:06 -0500

    CH2Player L5DrawPlayer

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
-import pygame 
+import pygame
 
-from constants import * 
+from constants import *
 
 
 def main():
     pygame.init()
-
+    clock = pygame.time.Clock()
+    dt = 0
     print("Hell  from bootdev!")
     print("Starting Asteroids!")
     print("Screen width: " + str(SCREEN_WIDTH))
     print("Screen height: " + str(SCREEN_HEIGHT))
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    run=True
-    while run: 
+    run = True
+    while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False 
+                run = False
         screen.fill("black")
         pygame.display.flip()
-
+        dt = clock.tick(60) / 1000
+        print(dt)
     pygame.quit()
 
 
---
+++ player.py
@@ -0,0 +1,10 @@
+form circleshape import *
+from constants import *
+
+class player(CircleShape):
+    def __init__(self,x,y):
+        super().__init__(self,)
+        
+    
+             
+             2
